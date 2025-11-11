a6f1b4453fc7e5350cb14c2ec1e11905926310da
jangseoyeong <suhyoung030327@naver.com>
2025-11-11 13:08:41 +0900

    Feat: 삭제 버튼 제작

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,5 +1,6 @@
 import ProfileImage from '../common/profileImage/ProfileImage';
 import RelationBadge from '../common/badge/relationBadge/RelationBadge';
+import trashImage from '../../assets/trash.svg';
 import styles from './Card.module.css';
 
 function Card({
@@ -17,6 +18,9 @@ function Card({
             <p className={styles.sender}>From. <span className={styles.senderName}>{sender}</span></p>
             <RelationBadge badgeName={relationship}/>
           </div>
+          <button className={styles.deleteBtn}>
+            <img src={trashImage} alt="삭제 버튼" />
+          </button>
         </div>
         <div className={styles.content}>{content}</div>
         <p className={styles.createdAt}>{createdAt}</p>
