b3337baf1bafdf6be0141690c3472258be528372
jieunsse <jieunsse@gmail.com>
2025-11-13 12:51:47 +0900

    Chore : 리스트 페이지 레이아웃 설정 #74

--- src/pages/list/ListPage.jsx
+++ src/pages/list/ListPage.jsx
@@ -1,3 +1,15 @@
+import Header from '@components/common/header/Header.jsx';
+import Button from '@components/common/button/base/Button.jsx';
+
 export default function ListPage() {
-  return <div>ListPage</div>;
+  return (
+    <div>
+      <Header />
+      <section>
+        <h2>인기 롤링 페이퍼 🔥</h2>
+        <h2>최근에 만든 롤링 페이퍼 🌟</h2>
+        <Button title="나도 만들어보기" />
+      </section>
+    </div>
+  );
 }
