5376d49b5f6e59accc9b62867278e2c5caff4d01
seoyoung <nct830@naver.com>
2025-11-13 12:19:06 +0900

    Chore : 모달 JSDoc 작성

--- src/components/modal/ModalContent.jsx
+++ src/components/modal/ModalContent.jsx
@@ -1,5 +1,18 @@
 import styles from './ModalContent.module.css';
 
+/**
+ * 모달 본문 메시지 영역 컴포넌트.
+ *
+ * 긴 메시지도 스크롤 가능하도록 감싸준다.
+ *
+ * @component
+ * @param {Object} props - 컴포넌트 props
+ * @param {string} props.message - 표시할 메시지 내용
+ *
+ * @example
+ * <ModalContent message="정말 수고 많았어요! 내일 봬요 😊" />
+ */
+
 function ModalContent({ message }) {
   return (
     <>
--- src/components/modal/ModalFooter.jsx
+++ src/components/modal/ModalFooter.jsx
@@ -2,6 +2,20 @@ import { useState } from 'react';
 import styles from './ModalFooter.module.css';
 import Button from '@components/common/button/base/Button.jsx';
 
+/**
+ * 모달 하단의 버튼 영역 컴포넌트.
+ *
+ * 확인 버튼 클릭 시 `onClose` 콜백을 호출한다.
+ * 버튼 컴포넌트의 visualState 값을 state로 변경해 버튼과 마우스의 상호작용에 대한 디자인 설정
+ *
+ * @component
+ * @param {Object} props - 컴포넌트 props
+ * @param {Function} props.onClose - 버튼 클릭 시 실행되는 닫기 핸들러
+ *
+ * @example
+ * <ModalFooter onClose={() => setIsOpen(false)} />
+ */
+
 function ModalFooter({ onClose }) {
   const [visualState, setVisualState] = useState('enabled');
 
