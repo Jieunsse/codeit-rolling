04b1dc2525f4b1688fe4ab6912c63a0aa380456c
jieunsse <jieunsse@gmail.com>
2025-11-13 17:38:35 +0900

    Chore : 버튼 핸들러 연결 #74

--- src/pages/list/ListPage.jsx
+++ src/pages/list/ListPage.jsx
@@ -1,14 +1,21 @@
 import Header from '@components/common/header/Header.jsx';
 import Button from '@components/common/button/base/Button.jsx';
+import { useNavigate } from 'react-router-dom';
 
 export default function ListPage() {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/');
+  };
+
   return (
     <div>
       <Header />
       <section>
         <h2>인기 롤링 페이퍼 🔥</h2>
         <h2>최근에 만든 롤링 페이퍼 🌟</h2>
-        <Button title="나도 만들어보기" />
+        <Button title="나도 만들어보기" onClick={handleClick} />
       </section>
     </div>
   );
