fb165c7c1028cbd70fc52f496c331d60b2913d84
jieunsse <jieunsse@gmail.com>
2025-11-14 12:19:29 +0900

    Feat : 공유버튼 컴포넌트 한 개로 통일 #88

--- src/components/subHeader/components/emojiAddButton/EmojiAddButton.jsx
+++
@@ -1,3 +0,0 @@
-export default function EmojiAddButton() {
-  return <div>이모지 추가 버튼</div>;
-}
--- src/components/subHeader/components/shareButton/ShareButton.jsx
+++ src/components/subHeader/components/shareButton/ShareButton.jsx
@@ -1,3 +1,17 @@
-export default function ShareButton() {
-  return <div>공유 버튼</div>;
+import styles from './shareButton.module.css';
+import add_24 from '@components/assets/add-24.svg';
+
+export default function ShareButton({ icon }) {
+  const isAddIcon = icon === add_24;
+  const isIconOnly = !isAddIcon;
+
+  return (
+    <button
+      className={`${styles.addButton} ${isIconOnly ? styles.iconOnly : ''}`}
+    >
+      <img src={icon} alt="아이콘" className={styles.icon} />
+
+      {isAddIcon && <span className={styles.label}>추가</span>}
+    </button>
+  );
 }
