1acd915846e1512d97a4ec09d29bb2f2ed650369
jieunsse <jieunsse@gmail.com>
2025-11-14 16:40:21 +0900

    Feat : 서브 헤더 및 버튼 로직 추가 #88

--- src/components/subHeader/SubHeader.jsx
+++ src/components/subHeader/SubHeader.jsx
@@ -1,20 +1,42 @@
+import { lazy, Suspense } from 'react';
+import styles from './subHeader.module.css';
+import ShareButton from './components/shareButton/ShareButton.jsx';
 import add_24 from '@components/assets/add-24.svg';
 import shareButton from '@components/assets/shareButton.svg';
-import ShareButton from '@components/subHeader/components/shareButton/ShareButton.jsx';
-import styles from '@components/subHeader/subHeader.module.css';
+import EmojiPickerSkeleton from '@components/subHeader/components/emojiPickerSkeleton/EmojiPickerSkeleton.jsx';
 
-export default function SubHeader() {
+const EmojiPickerLazy = lazy(() => import('emoji-picker-react')); // Lazy Load
+
+export default function SubHeader({
+  title,
+  isOpen,
+  onAddEmoji,
+  onEmojiSelect,
+  pickerRef,
+}) {
   return (
     <section className={styles.subHeader}>
-      <h2 className={styles.title}>To. Ashely Kim</h2>
+      <h2 className={styles.title}>To. {title}</h2>
 
       <div className={styles.rightArea}>
         <div className={styles.profileImage}>프로필</div>
         <span className={styles.count}>n명이 작성했어요!</span>
-
         <div className={styles.badgeList}>뱃지 목록</div>
 
-        <ShareButton icon={add_24} />
+        <div className={styles.addButtonWrapper}>
+          <ShareButton icon={add_24} onClick={onAddEmoji} />
+
+          <div
+            ref={pickerRef}
+            className={styles.pickerWrapper}
+            style={{ display: isOpen ? 'block' : 'none' }}
+          >
+            <Suspense fallback={<EmojiPickerSkeleton />}>
+              <EmojiPickerLazy onEmojiClick={onEmojiSelect} />
+            </Suspense>
+          </div>
+        </div>
+
         <ShareButton icon={shareButton} />
       </div>
     </section>
