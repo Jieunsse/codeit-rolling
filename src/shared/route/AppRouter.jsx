3a25cbf06140736f0e72fae42253d21aba845599
jangseoyeong <suhyoung030327@naver.com>
2025-11-14 12:55:40 +0900

    Chore: stash 작업 재개

--- src/shared/route/AppRouter.jsx
+++ src/shared/route/AppRouter.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from '@pages/home/Home.jsx';
+import Home from '@/pages/home/Home.jsx';
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/post/:recipientId" element={<PostIdPage />} />
       </Routes>
     </BrowserRouter>
   );
