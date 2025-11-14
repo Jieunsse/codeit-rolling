1d4462c7aa65b339920acacdb544b60716fee6a9
jieunsse <jieunsse@gmail.com>
2025-11-14 15:32:03 +0900

    Feat : SubHeader 레이아웃 추가 #88

--- src/components/subHeader/SubHeader.jsx
+++ src/components/subHeader/SubHeader.jsx
@@ -1,3 +1,22 @@
+import add_24 from '@components/assets/add-24.svg';
+import shareButton from '@components/assets/shareButton.svg';
+import ShareButton from '@components/subHeader/components/shareButton/ShareButton.jsx';
+import styles from '@components/subHeader/subHeader.module.css';
+
 export default function SubHeader() {
-  return <div>SubHeader</div>;
+  return (
+    <section className={styles.subHeader}>
+      <h2 className={styles.title}>To. Ashely Kim</h2>
+
+      <div className={styles.rightArea}>
+        <div className={styles.profileImage}>프로필</div>
+        <span className={styles.count}>n명이 작성했어요!</span>
+
+        <div className={styles.badgeList}>뱃지 목록</div>
+
+        <ShareButton icon={add_24} />
+        <ShareButton icon={shareButton} />
+      </div>
+    </section>
+  );
 }
