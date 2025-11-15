ce290441fbc3f973739915c3e8dd9a0ec97e1efc
jieunsse <jieunsse@gmail.com>
2025-11-16 01:40:36 +0900

    Feat : 구글로그인 임시 구현

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -1,6 +1,7 @@
 import logo from '@/components/assets/logo.svg';
 import styles from '@components/common/header/header.module.css';
 import { Link } from 'react-router-dom';
+import GoogleLoginButton from '@components/loginButton/GoogleLoginButton.jsx';
 
 export default function Header({ isButton }) {
   return (
@@ -9,10 +10,14 @@ export default function Header({ isButton }) {
         <Link to="/" className={styles.logo}>
           <img src={logo} alt="logo" />
         </Link>
+
         {isButton && (
-          <Link to="/post">
-            <button className={styles.button}>롤링 페이퍼 만들기</button>
-          </Link>
+          <div className={styles.leftBox}>
+            <GoogleLoginButton />
+            <Link to="/post">
+              <button className={styles.button}>롤링 페이퍼 만들기</button>
+            </Link>
+          </div>
         )}
       </div>
     </header>
