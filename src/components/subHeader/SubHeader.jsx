4a79b56d8c6d9ea1d504525990e6a9e7c1ce2241
jieunsse <jieunsse@gmail.com>
2025-11-15 09:54:54 +0900

    Chore : 리팩토링하며 변경된 부분 삭제 #88

--- src/components/subHeader/SubHeader.jsx
+++ src/components/subHeader/SubHeader.jsx
@@ -1,101 +1,75 @@
-import { lazy, Suspense } from 'react';
 import styles from './subHeader.module.css';
-import ShareButton from './components/shareButton/ShareButton.jsx';
-import add_24 from '@components/assets/add-24.svg';
-import shareButton from '@components/assets/shareButton.svg';
-import EmojiPickerSkeleton from '@components/subHeader/components/emojiPickerSkeleton/EmojiPickerSkeleton.jsx';
-import BadgeWrapper from '@components/subHeader/components/badgeWrapper/BadgeWrapper.jsx';
-import arrowDown from '@components/assets/arrow_down.svg';
-import PopoverMenu from '@components/subHeader/components/popoverMenu/PopoverMenu.jsx';
-
-const EmojiPickerLazy = lazy(() => import('emoji-picker-react'));
+import Toast from '@components/toast/Toast.jsx';
+import { useSubHeader } from './hooks/useSubHeader.js';
+import { useShare } from './hooks/useShare.js';
+import { useTopEmojis } from './hooks/useTopEmojis.js';
+import ShareOptionsPopover from '@components/subHeader/components/shareOptionsPopover/ShareOptionsPopover.jsx';
+import EmojiPickerPopover from '@components/subHeader/components/emoji/emojiPicker/EmojiPicker.jsx';
+import EmojiListPopover from '@components/subHeader/components/emoji/emojiListPopover/EmojiListPopover.jsx';
+import EmojiWrapper from '@components/subHeader/components/emoji/emojiWrapper/EmojiWrapper.jsx';
 
 export default function SubHeader({
   title,
-  emojiPicker,
-  arrowPopover,
-  sharePopover,
-  onClick,
+  emojiRanking = [],
+  onSelectEmoji,
+  onShare,
 }) {
-  const { isOpen, onAddEmoji, onEmojiSelect, pickerRef, buttonRef } =
-    emojiPicker;
+  const {
+    isOpen,
+    pickerRef,
+    emojiButtonRef,
+    togglePicker,
 
-  const { isArrowOpen, onToggleArrowPopover, arrowPopoverRef, arrowButtonRef } =
-    arrowPopover;
+    isArrowOpen,
+    arrowPopoverRef,
+    arrowButtonRef,
+    onToggleArrowPopover,
 
-  const { isShareOpen, onToggleSharePopover, sharePopoverRef, shareButtonRef } =
-    sharePopover;
+    isShareOpen,
+    sharePopoverRef,
+    shareButtonRef,
+    onToggleSharePopover,
+  } = useSubHeader();
+
+  const { showToast, handleShare } = useShare(onShare);
+  const topEmojis = useTopEmojis(emojiRanking);
 
   return (
-    <section className={styles.subHeader}>
-      <h2 className={styles.title}>To. {title}</h2>
+    <>
+      <section className={styles.subHeader}>
+        <h2 className={styles.title}>To. {title}</h2>
 
-      <div className={styles.rightArea}>
-        <div className={styles.profileImage}>프로필</div>
-        <span className={styles.count}>n명이 작성했어요!</span>
+        <span className={styles.rightArea}>
+          <div className={styles.profileImage} />
+          <span className={styles.count}>n명이 작성했어요!</span>
+          <EmojiWrapper topEmojis={topEmojis} />
 
-        <BadgeWrapper />
-
-        <div className={styles.arrowDownWrapper}>
-          <ShareButton
-            icon={arrowDown}
-            onClick={onToggleArrowPopover}
-            ref={arrowButtonRef}
+          <EmojiListPopover
+            isOpen={isArrowOpen}
+            popoverRef={arrowPopoverRef}
+            buttonRef={arrowButtonRef}
+            onToggle={onToggleArrowPopover}
+            topEmojis={topEmojis}
+          />
+          <EmojiPickerPopover
+            isOpen={isOpen}
+            pickerRef={pickerRef}
+            buttonRef={emojiButtonRef}
+            onToggle={togglePicker}
+            onSelectEmoji={onSelectEmoji}
           />
 
-          <div
-            ref={arrowPopoverRef}
-            className={`${styles.popover} ${
-              isArrowOpen ? styles.visible : styles.hidden
-            }`}
-          >
-            <PopoverMenu
-              items={[
-                { label: <BadgeWrapper />, value: 'badge1' },
-                { label: <BadgeWrapper />, value: 'badge2' },
-              ]}
-              onSelect={item => console.log('선택한 옵션:', item)}
-            />
-          </div>
-        </div>
-
-        <div className={styles.addButtonWrapper}>
-          <ShareButton icon={add_24} onClick={onAddEmoji} ref={buttonRef} />
-
-          <div
-            ref={pickerRef}
-            className={`${styles.pickerWrapper} ${
-              isOpen ? styles.visible : styles.hidden
-            }`}
-          >
-            <Suspense fallback={<EmojiPickerSkeleton />}>
-              <EmojiPickerLazy onEmojiClick={onEmojiSelect} />
-            </Suspense>
-          </div>
-        </div>
-
-        <div className={styles.shareWrapper}>
-          <ShareButton
-            icon={shareButton}
-            onClick={onToggleSharePopover}
-            ref={shareButtonRef}
+          <ShareOptionsPopover
+            isOpen={isShareOpen}
+            popoverRef={sharePopoverRef}
+            buttonRef={shareButtonRef}
+            onToggle={onToggleSharePopover}
+            onShare={handleShare}
           />
+        </span>
+      </section>
 
-          <div
-            ref={sharePopoverRef}
-            className={`${styles.popover} ${
-              isShareOpen ? styles.visible : styles.hidden
-            }`}
-          >
-            <button className={styles.popoverItem} onClick={onClick}>
-              카카오톡 공유
-            </button>
-            <button className={styles.popoverItem} onClick={onClick}>
-              URL 공유
-            </button>
-          </div>
-        </div>
-      </div>
-    </section>
+      {showToast && <Toast />}
+    </>
   );
 }
