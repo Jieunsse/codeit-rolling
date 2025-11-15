102de433ac1c03fb9d033d2a298295d75e9f8cde
jieunsse <jieunsse@gmail.com>
2025-11-15 09:54:12 +0900

    Chore : 변경된 파일 삭제 #88

---
+++ src/components/subHeader/components/shareOptionsPopover/ShareOptionsPopover.jsx
@@ -0,0 +1,33 @@
+// components/sharePopover/ShareOptionsPopover.jsx
+import styles from './sharePopover.module.css';
+import ShareButton from '../shareButton/ShareButton.jsx';
+import shareIcon from '@components/assets/shareButton.svg';
+
+export default function ShareOptionsPopover({
+  isOpen,
+  popoverRef,
+  buttonRef,
+  onToggle,
+  onShare,
+}) {
+  return (
+    <div className={styles.shareWrapper}>
+      <ShareButton icon={shareIcon} onClick={onToggle} ref={buttonRef} />
+
+      <div
+        ref={popoverRef}
+        className={`${styles.sharePopover} ${
+          isOpen ? styles.visible : styles.hidden
+        }`}
+      >
+        <button className={styles.popoverItem} onClick={() => onShare('kakao')}>
+          카카오톡 공유
+        </button>
+
+        <button className={styles.popoverItem} onClick={() => onShare('url')}>
+          URL 공유
+        </button>
+      </div>
+    </div>
+  );
+}
