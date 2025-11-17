5a467c5186cd8ba44605765000e7f10216dc71e3
jangseoyeong <suhyoung030327@naver.com>
2025-11-17 16:46:18 +0900

    Chore: 코드 정리

--- src/pages/postId/PostIdPage.jsx
+++ src/pages/postId/PostIdPage.jsx
@@ -35,7 +35,6 @@ function PostIdPage() {
   };
 
   const handleEmojiSelect = useCallback(async (emojiData) => {
-    
     try {
       const selectedEmoji = emojiData; 
       
@@ -45,9 +44,6 @@ function PostIdPage() {
       }
 
       const newReaction = await createReaction(recipientId, selectedEmoji);
-      
-      console.log("리액션 성공:", newReaction);
-      
       setRefreshReactions(prev => prev + 1); 
       
     } catch (error) {
@@ -71,9 +67,6 @@ function PostIdPage() {
   };
 
   const handleDeleteRecipient = async () => {
-    const recipientName = recipientData.name; 
-    const myName = localStorage.getItem('my_name');
-
     if (myName !== recipientName) {
       alert("이 롤링페이퍼를 삭제할 권한이 없습니다.");
       return;
@@ -84,7 +77,7 @@ function PostIdPage() {
       return;
     }
     if (String(inputId) !== String(recipientId)) {
-      alert("고유 ID가 일치하지 않습니다.");
+      alert("비밀번호가 일치하지 않습니다.");
       return;
     }
     if (window.confirm("롤링페이퍼를 삭제하시겠습니까?")) {
@@ -152,8 +145,6 @@ function PostIdPage() {
   };
 
   
-
-
   return(
     <div className={styles.pageBackground} style={pageStyle}>
       <div className={styles.headerWrap}>
